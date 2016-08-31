5d183e9bee59f07624f3f09f11e433fee03478bb
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-31 13:24:01 +0200

    Update test to version 4.0

--- tests/test_stock_shipment_return.py
+++ tests/test_stock_shipment_return.py
@@ -1,25 +1,16 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-import doctest
 import unittest
+import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_shipment_return')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_shipment_return')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_shipment_return'
 
 
 def suite():
@@ -28,5 +19,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_stock_shipment_in_return.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
